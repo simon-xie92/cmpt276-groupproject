d4370180444ea11997ff1fa2fd78841a13c59185
simon-xie92 <134343915+simon-xie92@users.noreply.github.com>
2023-06-28 18:57:34 -0700

    first

---
+++ src/test/java/com/cmpt276/groupproject/GroupprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.groupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
