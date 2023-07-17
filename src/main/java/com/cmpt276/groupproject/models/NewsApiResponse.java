ac5ae7dd91947576d54b4a0aeaf20433bd51cab4
Sandeep Singh <98441063+S-andeep-S@users.noreply.github.com>
2023-07-17 07:06:10 -0700

    Add news Api
    Added news Api, haven't done the front end, but you can access it at http://localhost:8080/news

---
+++ src/main/java/com/cmpt276/groupproject/models/NewsApiResponse.java
@@ -0,0 +1,15 @@
+package com.cmpt276.groupproject.models;
+
+public class NewsApiResponse {
+
+    private Article[] articles;
+
+    public Article[] getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Article[] articles) {
+        this.articles = articles;
+    }
+
+}
